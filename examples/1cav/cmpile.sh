e952e52d69399d8058212f55dacb822d5ff9a2fe
ricardofrantz <rfrantz@gmail.com>
2020-04-16 14:54:41 +0200

    changing standard options to gcc

--- examples/1cav/cmpile.sh
+++ examples/1cav/cmpile.sh
@@ -4,13 +4,12 @@ export CASE="cav" #--> case name goes here
 
 #add to .bashrc
 export NEKSTAB_SOURCE_ROOT="$HOME/nekStab" #--> path to nekStab
-#export NEK_SOURCE_ROOT="$NEKSTAB_SOURCE_ROOT/Nek5000"
-export NEK_SOURCE_ROOT="$HOME/nekStab2/Nek5000"
+export NEK_SOURCE_ROOT="$NEKSTAB_SOURCE_ROOT/Nek5000"
 export PATH=$NEK_SOURCE_ROOT/bin:$PATH
 
 #uncomment the deisred compiler
-#source ${NEKSTAB_SOURCE_ROOT}/core/compiler_gcc.sh
-source ${NEKSTAB_SOURCE_ROOT}/core/compiler_intel.sh
+source ${NEKSTAB_SOURCE_ROOT}/core/compiler_gcc.sh
+#source ${NEKSTAB_SOURCE_ROOT}/core/compiler_intel.sh
 
 
 args=("$@")
