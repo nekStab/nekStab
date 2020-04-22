200acd23ff2876fe7768589c64ddfc1327eab47f
ricardofrantz <rfrantz@gmail.com>
2020-04-22 18:14:00 +0200

    new compiler option

---
+++ core/compiler_pgi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export PGI=/opt/pgi;
+export PATH=/opt/pgi/linux86-64/19.10/bin:$PATH;
+export MANPATH=$MANPATH:/opt/pgi/linux86-64/19.10/man;
+export LM_LICENSE_FILE=$LM_LICENSE_FILE:/opt/pgi/license.dat; 
+
+## PGI COMPILER
+source ${NEKSTAB_SOURCE_ROOT}/core/nekStab.sh
+export FC="pgfortran"
+export CC="pgcc"
+export FFLAGS="-mcmodel=medium -g -traceback"
+export USR_LFLAGS+="-llapack -lblas"
+
+source ${NEKSTAB_SOURCE_ROOT}/core/nekStab.sh
