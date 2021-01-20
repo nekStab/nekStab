4e6f3175aa655a22ab781f5eb6f1dd768a60d297
ricardofrantz <rasfrantz@gmail.com>
2021-01-20 17:07:38 +0100

    adding square cylinder case

---
+++ examples/sq_cyl/cmpile.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export CASE="square" #--> case name goes here
+
+#add to .bashrc
+export NEKSTAB_SOURCE_ROOT="$HOME/nekStab" #--> path to nekStab
+export NEK_SOURCE_ROOT="$NEKSTAB_SOURCE_ROOT/Nek5000"
+export PATH=$NEK_SOURCE_ROOT/bin:$PATH
+
+#uncomment the deisred compiler
+source ${NEKSTAB_SOURCE_ROOT}/core/compiler_gcc.sh
+#source ${NEKSTAB_SOURCE_ROOT}/core/compiler_intel.sh
+#source ${NEKSTAB_SOURCE_ROOT}/core/compiler_pgi.sh
+
+
+args=("$@")
+argsnr=$#
+
+if [ $[argsnr] -ne 1 ]; then
+    error_quit 'Wrong arguments number!'
+fi
+
+for il in "$@"
+do
+case $il in
+        clean)
+                ${SOURCE_ROOT}/bin/makenek clean
+                shift
+                ;;
+        all)
+                ${SOURCE_ROOT}/bin/makenek ${CASE}
+                shift
+                ;;
+esac
+done
