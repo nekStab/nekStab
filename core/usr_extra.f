7fa3362f58334e9d3f4ce0440936b46705d0aed7
Jean-Christophe <loiseau.jc@gmail.com>
2021-03-23 17:25:18 +0100

    Update usr_extra.f
    Added the Newton-Krylov case.

--- core/usr_extra.f
+++ core/usr_extra.f
@@ -26,29 +26,31 @@ c-----------------------------------------------------------------------
             call spng_init
          endif
 
-       !applying sponge to the BM1 matrix to remove the sponge zone from the eigensolver
-       call copy(bm1s, bm1, n)
-       if(uparam(10).gt.0)then !sponge on
-        do i=1,n
-         if( spng_fun( i ) .gt. 0 ) bm1s( i,1,1,1 )=0.0d0
-        enddo
-        endif
-       !call outpost(bm1,bm1s,wo3,pr,t,'BM1')
+!     applying sponge to the BM1 matrix to remove the sponge zone from the eigensolver
+         call copy(bm1s, bm1, n)
+         if(uparam(10).gt.0)then !sponge on
+            do i=1,n
+               if( spng_fun( i ) .gt. 0 ) bm1s( i,1,1,1 )=0.0d0
+            enddo
+         endif
+!     call outpost(bm1,bm1s,wo3,pr,t,'BM1')
 
       endif
+
       call oprzero(fcx,fcy,fcz) ! never comment this!
       call rzero(fct,n)
 
       if(if3d.AND.ifbf2d)then
-       if(nid.eq.0)write(6,*)' Forcing 2D flow -> vz=0'
-       call rzero(vz,nx1*ny1*nz1*nelv)
+         if(nid.eq.0)write(6,*)' Forcing 2D flow -> vz=0'
+         call rzero(vz,nx1*ny1*nz1*nelv)
       endif
 
       if(uparam(1).le.2)then
 
+
          if(istep.eq.0)then
 
-            !if(nid.eq.0)open(unit=11,file='stats.dat')
+!     if(nid.eq.0)open(unit=11,file='stats.dat')
             ifbfcv=.false.
 
             re = 1.0d0/param(2) !inital value
@@ -64,7 +66,7 @@ c-----------------------------------------------------------------------
                write(6,*)' eek =',eek
             endif
 
-         endif !istep.eq.0
+         endif                  !istep.eq.0
 
          if((istep.eq.0).OR.ifoutfld)then
 
@@ -79,35 +81,39 @@ c-----------------------------------------------------------------------
          if(istep.eq.0)call outpost(vx,vy,vz,pr,t,'   ')
 
 
-        ! if (mod(istep,10).eq.0) then
-        !    uek = glsc3(vx,bm1,vx,n)*eek
-        !    vek = glsc3(vy,bm1,vy,n)*eek
-        !    if(if3d)then
-        !       wek = glsc3(vz,bm1,vz,n)*eek
-        !       write(11,"(5E15.7)")time,uek,vek,wek,uek+vek+wek
-        !    else
-        !       write(11,"(4E15.7)")time,uek,vek,uek+vek
-        !    endif
-        ! endif
-        !if(istep.eq.lastep)close(11)
+!     if (mod(istep,10).eq.0) then
+!     uek = glsc3(vx,bm1,vx,n)*eek
+!     vek = glsc3(vy,bm1,vy,n)*eek
+!     if(if3d)then
+!     wek = glsc3(vz,bm1,vz,n)*eek
+!     write(11,"(5E15.7)")time,uek,vek,wek,uek+vek+wek
+!     else
+!     write(11,"(4E15.7)")time,uek,vek,uek+vek
+!     endif
+!     endif
+!     if(istep.eq.lastep)close(11)
 
-        call hpts
+         call hpts
 
-         if(uparam(01).eq.1)then !compose forcings to fcx,fcy,fcz
-            !if(uparam(03).eq.1)call sfd
-            if(uparam(03).eq.1)call sfd_ab3
+         if(uparam(01) .eq. 1) then !compose forcings to fcx,fcy,fcz
+            if(uparam(03).eq.1) then
+               call sfd_ab3
+            elseif(uparam(03).eq.3) then
+               call newton_krylov
+               call nek_end
+            endif
          endif
 
          call mycomment
 
-         if( ifbfcv )then ! after converging base fow...
+         if( ifbfcv )then       ! after converging base fow...
 
-         !call switch_to_lnse_steady !in utilities.f
-         !call krylov_schur ! in eigensolvers.f
-         if(nid.eq.0)write(6,*)'Stopping code...'
-         call nek_end
+!     call switch_to_lnse_steady !in utilities.f
+!     call krylov_schur ! in eigensolvers.f
+            if(nid.eq.0)write(6,*)'Stopping code...'
+            call nek_end
 
-         endif !ifbfcv
+         endif                  !ifbfcv
 
       elseif(uparam(01).ge.3)then !3:direct,4:adj,5:dir-adj,6:adj-dir
 
@@ -119,12 +125,12 @@ c-----------------------------------------------------------------------
          param(12) = -abs(param(12)) !freeze dt
          param(31) = 1 ; npert = param(31)
          call bcast(param,200*wdsize) !broadcast all parameters to processors
-         call krylov_schur ! in eigensolvers.f
+         call krylov_schur      ! in eigensolvers.f
          if(nid.eq.0)write(6,*)'Stopping code...'
          call nek_end
 
       endif
-      
+
       return
       end
 c-----------------------------------------------------------------------
@@ -136,23 +142,23 @@ c-----------------------------------------------------------------------
       real, save :: deltatime
       real telapsed,tpernondt,tmiss
 
-      !if extrapolation is not OIFS -> ifchar = false
-      !if OIFS active -> ifchar = .true. and CFL 2-5
-      !some cases can have CFL>1 in initial time steps
+!     if extrapolation is not OIFS -> ifchar = false
+!     if OIFS active -> ifchar = .true. and CFL 2-5
+!     some cases can have CFL>1 in initial time steps
       if (courno.gt.10.0d0) then
-        if (nio.eq.0)then
-          write(6,*)
-          write(6,*)'    CFL > 10 stopping'
-          write(6,*)
+         if (nio.eq.0)then
+            write(6,*)
+            write(6,*)'    CFL > 10 stopping'
+            write(6,*)
          endif
-        call nek_end
+         call nek_end
       endif
 
       if (nio.ne.0) return
 
       if (eetime0.eq.0.0 .and. istep.eq.1)then
-        eetime0=dnekclock()
-        deltatime=time
+         eetime0=dnekclock()
+         deltatime=time
       endif
 
       eetime1=eetime2
@@ -160,46 +166,46 @@ c-----------------------------------------------------------------------
 
       if (istep.gt.0 .and. lastep.eq.0 .and. iftran) then
 
-        ttime_stp = eetime2-eetime1 ! time per timestep
-        ttime     = eetime2-eetime0 ! sum of all timesteps
+         ttime_stp = eetime2-eetime1 ! time per timestep
+         ttime     = eetime2-eetime0 ! sum of all timesteps
 
-        if(istep.eq.1)then
-          ttime_stp = 0.0d0; ttime = 0.0d0
-        endif
+         if(istep.eq.1)then
+            ttime_stp = 0.0d0; ttime = 0.0d0
+         endif
 
-        if (mod(istep,5).eq.0) then
+         if (mod(istep,5).eq.0) then
 
-          telapsed = ttime/3600.0d0
-          tpernondt = (ttime/(time-deltatime))
-          tmiss = (param(10)-time)*tpernondt/3600.0d0
+            telapsed = ttime/3600.0d0
+            tpernondt = (ttime/(time-deltatime))
+            tmiss = (param(10)-time)*tpernondt/3600.0d0
 
-          write(6,*)' '
-          write(6,103)1.d0/param(2)
-          write(6,102)ttime/istep,int(((ttime/istep)-ttime_stp)*1000) !to ms
-          write(6,104)int(telapsed),int((telapsed-int(telapsed))*60.)
-          write(6,108)int(tmiss),int((tmiss-int(tmiss))*60.)
-          if(tpernondt.gt.60.)then
-            write(6,105)tpernondt
-          else
-            write(6,106)tpernondt/60.0d0
-          endif
-          write(6,107)time-deltatime, int((time-deltatime)/param(14))+1 !,deltatime,param(10)
-          write(6,*)
-          write(6,*)
+            write(6,*)' '
+            write(6,103)1.d0/param(2)
+            write(6,102)ttime/istep,int(((ttime/istep)-ttime_stp)*1000) !to ms
+            write(6,104)int(telapsed),int((telapsed-int(telapsed))*60.)
+            write(6,108)int(tmiss),int((tmiss-int(tmiss))*60.)
+            if(tpernondt.gt.60.)then
+               write(6,105)tpernondt
+            else
+               write(6,106)tpernondt/60.0d0
+            endif
+            write(6,107)time-deltatime, int((time-deltatime)/param(14))+1 !,deltatime,param(10)
+            write(6,*)
+            write(6,*)
 
-          !write(6,*)'ramp, pert = ',1./(1.+exp(4.-0.25*time)),(1.+0.05*cos(time*8.*atan(1.)*uparam(8)))
+!     write(6,*)'ramp, pert = ',1./(1.+exp(4.-0.25*time)),(1.+0.05*cos(time*8.*atan(1.)*uparam(8)))
 
-        endif
+         endif
 
       endif
 
-  102 format('      Mean time per timestep: ',F8.4,'  dev:',I8,'ms')
-  103 format('      Re=',F8.2,F8.2)
-  104 format('      Elapsed time: ',I8,' h ',I2,' min')
-  105 format('      Time per nondimensional time: ',F8.2,' sec')
-  106 format('      Time per nondimensional time: ',F8.2,' min ')
-  107 format('      Local time: ',F8.4,'  File:',I8) !,' StartFrom=',F8.4,'endTime=',F8.4)
-  108 format('      Remaining time: ',I8,' h ',I2,' min')
+ 102  format('      Mean time per timestep: ',F8.4,'  dev:',I8,'ms')
+ 103  format('      Re=',F8.2,F8.2)
+ 104  format('      Elapsed time: ',I8,' h ',I2,' min')
+ 105  format('      Time per nondimensional time: ',F8.2,' sec')
+ 106  format('      Time per nondimensional time: ',F8.2,' min ')
+ 107  format('      Local time: ',F8.4,'  File:',I8) !,' StartFrom=',F8.4,'endTime=',F8.4)
+ 108  format('      Remaining time: ',I8,' h ',I2,' min')
 
       return
       end
@@ -215,7 +221,7 @@ c-----------------------------------------------------------------------
 
       integer e,eg,eg0,e0
 
-      eg0 = 622          ! Identify element/processor in wake
+      eg0 = 622                 ! Identify element/processor in wake
       mid = gllnid(eg0)
       e0  = gllel (eg0)
 
@@ -294,7 +300,7 @@ c-----------------------------------------------------------------------
       endif
       end
 c-----------------------------------------------------------------------
-      subroutine set_rjet(ub) !round jet profile for axissymetric jet
+      subroutine set_rjet(ub)   !round jet profile for axissymetric jet
       include 'SIZE'
       include 'TOTAL'
       real ub(1),theta_0
@@ -320,15 +326,15 @@ c-----------------------------------------------------------------------
       x_m = uparam(07)
 
       do i=1,n
-      x = xm1(i,1,1,1)
-      y = ym1(i,1,1,1)
-      if(y.eq.0.)then
-         H = exp( -((x-x_m)**2)/(3.10d0**2))
-         f_x = 15.18750d0*H**5 -35.43750d0*H**4 +20.250d0*H**3
-         v_jet(i)=A_1*f_x
-      else
-         v_jet(i)=0.0d0
-      endif
+         x = xm1(i,1,1,1)
+         y = ym1(i,1,1,1)
+         if(y.eq.0.)then
+            H = exp( -((x-x_m)**2)/(3.10d0**2))
+            f_x = 15.18750d0*H**5 -35.43750d0*H**4 +20.250d0*H**3
+            v_jet(i)=A_1*f_x
+         else
+            v_jet(i)=0.0d0
+         endif
       enddo
 
       return
@@ -344,7 +350,7 @@ c-----------------------------------------------------------------------
       real ampx, ampy, glamax
       integer n
       logical ifto_sav, ifpo_sav
-      
+
       n = nx1*ny1*nz1*nelv
 
       if((istep.eq.0).OR.ifoutfld)then
@@ -355,146 +361,146 @@ c-----------------------------------------------------------------------
          call outpost( do1,do2,do3,pr,t,'per')
          ifto = ifto_sav ; ifpo = ifpo_sav
 
-         ampx = glamax(do1,n)      
+         ampx = glamax(do1,n)
          ampy = glamax(do2,n)
 
          if(nid.eq.0)then
             if(istep.eq.0)then
-            open(unit=111,file='ts_amp.dat',status='unknown',form='formatted')
-            write(112,'(A)')'#  t  A  up  vp  up2  vp2'
+               open(unit=111,file='ts_amp.dat',status='unknown',form='formatted')
+               write(112,'(A)')'#  t  A  up  vp  up2  vp2'
             endif
             write(111,"(6E15.7)")time,uparam(06),ampx,ampy,ampx**2,ampy**2
             if(istep.eq.nsteps)close(111)
          endif
-      endif      
+      endif
       return
       end
 c-----------------------------------------------------------------------
       subroutine set_blasius(ub,vb) ! Compute the Blasius profile
-         include 'SIZE'
-         include 'TOTAL'
+      include 'SIZE'
+      include 'TOTAL'
 
-         real ub(1),vb(1),x_m
-         real,save :: xmx,xmn
-         n = nx1*ny1*nz1*nelv
+      real ub(1),vb(1),x_m
+      real,save :: xmx,xmn
+      n = nx1*ny1*nz1*nelv
 
-         xmx = glmax(xm1,n)
-         xmn = glmin(xm1,n)
-         x_m = uparam(07)
+      xmx = glmax(xm1,n)
+      xmn = glmin(xm1,n)
+      x_m = uparam(07)
 
-         visc = param(2)/param(1) !density / dynamic viscosity
-         delta99_0 = 5.0d0/1.72080d0 !2.9
-         delta_star= 1.0d0
-         u_0   = 1.0d0
-         x_0 = (delta_star/1.7208d0)**2 / visc * u_0  ! Reference x
+      visc = param(2)/param(1)  !density / dynamic viscosity
+      delta99_0 = 5.0d0/1.72080d0 !2.9
+      delta_star= 1.0d0
+      u_0   = 1.0d0
+      x_0 = (delta_star/1.7208d0)**2 / visc * u_0 ! Reference x
 
-         x_inflow = (605.0d0/740.0d0)**2 * x_0 !original blasius
-         x_inflow = x_0
+      x_inflow = (605.0d0/740.0d0)**2 * x_0 !original blasius
+      x_inflow = x_0
 
-         if(nid.eq.0)then
+      if(nid.eq.0)then
          write(6,*)'visc=',visc
          write(6,*)'delta99_0=',delta99_0
          write(6,*)'delta_star=',delta_star
          write(6,*)'u_0=',u_0
-         write(6,*)'x_0=',x_0 
+         write(6,*)'x_0=',x_0
          write(6,*)'x_inflow=',x_inflow
          write(6,*)'x_m=',x_m
-         endif
+      endif
 
-         do i=1,n
-            x = xm1(i,1,1,1)
-            y = ym1(i,1,1,1)
+      do i=1,n
+         x = xm1(i,1,1,1)
+         y = ym1(i,1,1,1)
 
-            x_t = x_inflow + x
-            rex = u_0 * x_t / visc
+         x_t = x_inflow + x
+         rex = u_0 * x_t / visc
 
-            if(x.eq.xmn)write(6,*)'if x,rex=',real(x,4),real(rex,4),real(sqrt(rex),4)
-            if(x.eq.x_m)write(6,*)'sb x,rex=',real(x,4),real(rex,4),real(sqrt(rex),4)
-            if(x.eq.xmx)write(6,*)'of x,rex=',real(x,4),real(rex,4),real(sqrt(rex),4)
-                     
-            eta = y*sqrt(rex)/x_t
-            call blasius(ub(i),vb(i),eta,rex)
+         if(x.eq.xmn)write(6,*)'if x,rex=',real(x,4),real(rex,4),real(sqrt(rex),4)
+         if(x.eq.x_m)write(6,*)'sb x,rex=',real(x,4),real(rex,4),real(sqrt(rex),4)
+         if(x.eq.xmx)write(6,*)'of x,rex=',real(x,4),real(rex,4),real(sqrt(rex),4)
 
-         enddo
+         eta = y*sqrt(rex)/x_t
+         call blasius(ub(i),vb(i),eta,rex)
 
-         return
-         end
+      enddo
+
+      return
+      end
 c-----------------------------------------------------------------------
       subroutine blasius(u,v,eta,rex)
 
-         integer icalld
-         save    icalld
-         data    icalld /0/
+      integer icalld
+      save    icalld
+      data    icalld /0/
 
-         parameter (lb=100)
-         real blasius_soln(0:4,0:lb)
-         save blasius_soln
-c
+      parameter (lb=100)
+      real blasius_soln(0:4,0:lb)
+      save blasius_soln
+c     
 c     Algorithm found in Lighthills monograph on mathematical fluid mech.
 c     (c. of M. Choudhari)
-c
-         real  w(4)
+c     
+      real  w(4)
 
-         twok2 =  1.6551903602308323382003140460740
-         rk2   =  0.5*twok2
-         rk    =  sqrt(rk2)
+      twok2 =  1.6551903602308323382003140460740
+      rk2   =  0.5*twok2
+      rk    =  sqrt(rk2)
 
-         if (icalld.eq.0) then
-            icalld = 1
+      if (icalld.eq.0) then
+         icalld = 1
 
-            call set_ics (blasius_soln(0,0))
+         call set_ics (blasius_soln(0,0))
 
-            dt=.05
-            rr=1.0725
-            do i=1,lb
-               blasius_soln(0,i) = blasius_soln(0,i-1) + dt
-               dt = dt*rr
-            enddo
+         dt=.05
+         rr=1.0725
+         do i=1,lb
+            blasius_soln(0,i) = blasius_soln(0,i-1) + dt
+            dt = dt*rr
+         enddo
 
-            do i=1,lb
-               eta0 = blasius_soln(0,i-1)
-               eta1 = blasius_soln(0,i  )
-               t0 = 0.5*eta0/rk
-               t1 = 0.5*eta1/rk
-               dt = .0005    !  Note, this is good to about 12 digits
-               m=3
-               call copy(blasius_soln(1,i),blasius_soln(1,i-1),m)
-               call rk4_integrate(blasius_soln(1,i),3,t1,t0,dt)
-            enddo
-         endif
+         do i=1,lb
+            eta0 = blasius_soln(0,i-1)
+            eta1 = blasius_soln(0,i  )
+            t0 = 0.5*eta0/rk
+            t1 = 0.5*eta1/rk
+            dt = .0005          !  Note, this is good to about 12 digits
+            m=3
+            call copy(blasius_soln(1,i),blasius_soln(1,i-1),m)
+            call rk4_integrate(blasius_soln(1,i),3,t1,t0,dt)
+         enddo
+      endif
 
-         if (eta.gt.blasius_soln(0,lb)) then
+      if (eta.gt.blasius_soln(0,lb)) then
 
-            call copy(w,blasius_soln(1,lb),2)
+         call copy(w,blasius_soln(1,lb),2)
 
-         else
+      else
 
-            i = interval_find(eta,blasius_soln,5,lb)
+         i = interval_find(eta,blasius_soln,5,lb)
 
-            eta0 = blasius_soln(0,i)
-            t0   = 0.5*eta0/rk
-            t1   = 0.5*eta/rk
-            dt   = .0005    !  Note, this is good to about 12 digits
-            m    = 3
-            call copy(w,blasius_soln(1,i),m)
-            call rk4_integrate(w,3,t1,t0,dt)
+         eta0 = blasius_soln(0,i)
+         t0   = 0.5*eta0/rk
+         t1   = 0.5*eta/rk
+         dt   = .0005           !  Note, this is good to about 12 digits
+         m    = 3
+         call copy(w,blasius_soln(1,i),m)
+         call rk4_integrate(w,3,t1,t0,dt)
 
-         endif
+      endif
 
-         g  = w(1)
-         gp = w(2)
+      g  = w(1)
+      gp = w(2)
 
-         f  = g  / rk
-         fp = gp / twok2
+      f  = g  / rk
+      fp = gp / twok2
 
-         u  = fp
-         v  = 0.5*(eta*fp-f)/sqrt(rex)
+      u  = fp
+      v  = 0.5*(eta*fp-f)/sqrt(rex)
 
 c     write(6,1) eta,u,v,f,fp,rex
-c  1  format(1p6e14.6,' eta')
+c     1  format(1p6e14.6,' eta')
 
-         return
-         end
+      return
+      end
 c-----------------------------------------------------------------------
       subroutine rk4_integrate(w,n,tfinal,tstart,dti) !Program to integrate dW/dt = F(W,t)
 !     Input:   w() is initial condition at t=tstart
@@ -510,7 +516,7 @@ c-----------------------------------------------------------------------
          dt     = tdelta/nsteps
 
          t = tstart
-         do k=1,nsteps !  TIME STEPPING
+         do k=1,nsteps          !  TIME STEPPING
 
             call rk4 (w,t,dt,n) ! Single RK4 step (nmax=4)
             t = t+dt
@@ -522,12 +528,12 @@ c-----------------------------------------------------------------------
       return
       end
 c-----------------------------------------------------------------------
-      subroutine set_ics (w)!Initial conditions for modified Blasius equation g''' + g g'' = 0
+      subroutine set_ics (w)    !Initial conditions for modified Blasius equation g''' + g g'' = 0
       real  w(0:3)
-      w(0) = 0.0d0    ! eta = 0
-      w(1) = 0.0d0    ! g
-      w(2) = 0.0d0    ! g'
-      w(3) = 1.0d0    ! g"
+      w(0) = 0.0d0              ! eta = 0
+      w(1) = 0.0d0              ! g
+      w(2) = 0.0d0              ! g'
+      w(3) = 1.0d0              ! g"
       return
       end
 c-----------------------------------------------------------------------
@@ -587,14 +593,14 @@ c-----------------------------------------------------------------------
 
          klo=0
          khi=n
-   1    if ((khi-klo).gt.1) then
-         k=(khi+klo)/2
-         if (xa(1,k).gt.x) then
-            khi=k
-         else
-            klo=k
-         endif
-         goto 1
+ 1       if ((khi-klo).gt.1) then
+            k=(khi+klo)/2
+            if (xa(1,k).gt.x) then
+               khi=k
+            else
+               klo=k
+            endif
+            goto 1
          endif
 
          h=xa(1,khi)-xa(1,klo)
