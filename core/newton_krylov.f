e7b5f7d56f72ec378c26a3c04de806b0a3850922
Jean-Christophe <loiseau.jc@gmail.com>
2021-03-25 16:53:49 +0100

    Update newton_krylov.f
    Cleaned-up the whole code and improved the implementation of the gmres solver.

--- core/newton_krylov.f
+++ core/newton_krylov.f
@@ -19,8 +19,8 @@
       integer, parameter :: lt2 = lx2*ly2*lz2*lelt
 
 ! ----- Right-hand side vector for Newton solver.
-      real, dimension(lt) :: fx, fy, fz, ft
-      real, dimension(lt2) :: fp
+      real, dimension(lt) :: f_x, f_y, f_z, f_t
+      real, dimension(lt2) :: f_p
 
 ! -----
       real, dimension(lt) :: qx, qy, qz, qt
@@ -44,9 +44,9 @@
       tol = 1e-8
 
 ! --> Initialize arrays.
-      call oprzero(fx, fy, fz)
-      call rzero(fp, n2)
-      call rzero(ft, n)
+      call oprzero(f_x, f_y, f_z)
+      call rzero(f_p, n2)
+      call rzero(f_t, n)
 
 ! --> Copy initial guess for the Newton solver.
       call opcopy(qx, qy, qz, vx, vy, vz)
@@ -60,16 +60,15 @@
          time = i
          call outpost(qx, qy, qz, qp, qt, "nwt")
 
-! --> Compute rhs of Newton iteration.
-         call forward_map(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
+! --> Compute rhs of Newton iteration f(q).
+         call forward_map(f_x, f_y, f_z, f_p, f_t, qx, qy, qz, qp, qt)
 
-! --> Check residual
-         call norm(fx, fy, fz, fp, ft, residual)
+! --> Check residual || f(q) ||
+         call norm(f_x, f_y, f_z, f_p, f_t, residual)
          residual = residual ** 2
 
          if (nid .eq. 0) then
-            write(6, *) "NEWTON ITERATION :", i
-            write(6, *) "NEWTON RESIDUAL :", residual
+            write(6, *) "NEWTON --- Iteration :", i, " residual :", residual
          endif
 
          if (residual .lt. max(param(21),param(22))) then
@@ -90,7 +89,7 @@
             param(31) = 1 ; npert = param(31)
             call bcast(param,200*wdsize) !broadcast all parameters to processors
 
-            call time_stepper_gmres(fx, fy, fz, fp, ft, dqx, dqy, dqz, dqp, dqt, maxiter_gmres)
+            call time_stepper_gmres(f_x, f_y, f_z, f_p, f_t, dqx, dqy, dqz, dqp, dqt, maxiter_gmres)
             ifpert = .false.
             call bcast(ifpert, lsize)
 
@@ -120,134 +119,131 @@
 
       subroutine time_stepper_gmres(rhs_x, rhs_y, rhs_z, rhs_p, rhs_t, sol_x, sol_y, sol_z, sol_p, sol_t, maxiter)
 
-!     Implementation of simple GMRES to be part of the Newton-Krylov solver
-!     for fixed point computation. The rank of the Krylov subspace is set as the user parameter k_dim.
-!
-!     INPUT
-!     -----
-!
-!     rhs_x, rhs_y, rhs_z, rhs_t : nek arrays of size (lt).
-!     Arrays containing the right-hand side of the linear problem to be solved.
-!
-!     rhs_p : nek array of size (lt2)
-!     Array containing the right-hand side of the linear problem to be solved (pressure component).
-!
-!     maxiter : integer
-!     Maximum number of restarts for the GMRES computation.
-!
-!     RETURNS
-!     -------
-!
-!     sol_x, sol_y, sol_z, sol_t : nek arrays of size (lt).
-!     Arrays containing the solution of the linear problem.
-!
-!     sol_p : nek array of size (lt2).
-!     Array containing the solution of the linear problem (pressure component).
-!
-!
-!     NOTE : This is a plain implementation of GMRES following the algorithm given in
-!     Y. Saad. Iterative methods for sparse linear systems. Section 6.5 GMRES, alg. 6.9
-!
-!     Last Edit : March 22nd 2021 by JC Loiseau.
-!
+        !     Implementation of simple GMRES to be part of the Newton-Krylov solver
+        !     for fixed point computation. The rank of the Krylov subspace is set as the user parameter k_dim.
+        !
+        !     INPUT
+        !     -----
+        !
+        !     rhs_x, rhs_y, rhs_z, rhs_t : nek arrays of size (lt).
+        !     Arrays containing the right-hand side of the linear problem to be solved.
+        !
+        !     rhs_p : nek array of size (lt2)
+        !     Array containing the right-hand side of the linear problem to be solved (pressure component).
+        !
+        !     maxiter : integer
+        !     Maximum number of restarts for the GMRES computation.
+        !
+        !     RETURNS
+        !     -------
+        !
+        !     sol_x, sol_y, sol_z, sol_t : nek arrays of size (lt).
+        !     Arrays containing the solution of the linear problem.
+        !
+        !     sol_p : nek array of size (lt2).
+        !     Array containing the solution of the linear problem (pressure component).
+        !
+        !
+        !     NOTE : This is a plain implementation of GMRES following the algorithm given in
+        !     Y. Saad. Iterative methods for sparse linear systems. Section 6.5 GMRES, alg. 6.9
+        !
+        !     Last Edit : March 22nd 2021 by JC Loiseau.
+        !
 
-      implicit none
-      include 'SIZE'
-      include 'TOTAL'
+        implicit none
+        include 'SIZE'
+        include 'TOTAL'
 
-      integer, parameter :: lt = lx1*ly1*lz1*lelt
-      integer, parameter :: lt2 = lx2*ly2*lz2*lelt
+        integer, parameter :: lt = lx1*ly1*lz1*lelt
+        integer, parameter :: lt2 = lx2*ly2*lz2*lelt
 
-!     ----- Right-hand side vector of A x = b -----
-      real, dimension(lt) :: rhs_x, rhs_y, rhs_z, rhs_t
-      real, dimension(lt2) :: rhs_p
+        !     ----- Right-hand side vector of A x = b -----
+        real, dimension(lt) :: rhs_x, rhs_y, rhs_z, rhs_t
+        real, dimension(lt2) :: rhs_p
 
-!     ----- GMRES solution vector.
-      real, dimension(lt) :: sol_x, sol_y, sol_z, sol_t
-      real, dimension(lt2) :: sol_p
+        !     ----- GMRES solution vector.
+        real, dimension(lt) :: sol_x, sol_y, sol_z, sol_t
+        real, dimension(lt2) :: sol_p
 
-!     ----- Krylov basis for the Arnoldi factorization.
-      real, dimension(lt, k_dim+1) :: qx, qy, qz, qt
-      real, dimension(lt2, k_dim+1) :: qp
+        !     ----- Krylov basis for the Arnoldi factorization.
+        real, dimension(lt, k_dim+1) :: qx, qy, qz, qt
+        real, dimension(lt2, k_dim+1) :: qp
 
-!     ----- Upper Hessenberg matrix.
-      real, dimension(k_dim+1, k_dim) :: H
-      real, dimension(k_dim) :: yvec
-      real, dimension(k_dim+1) :: evec
+        !     ----- Upper Hessenberg matrix.
+        real, dimension(k_dim+1, k_dim) :: H
+        real, dimension(k_dim) :: yvec
+        real, dimension(k_dim+1) :: evec
 
-!     ----- Miscellaneous.
-      integer :: i, j, k, maxiter, n, n2
-      logical :: converged
-      real :: beta, residual, tol, rhs_norm
+        !     ----- Miscellaneous.
+        integer :: i, j, k, maxiter, n, n2
+        logical :: converged
+        real :: beta, residual, tol, rhs_norm, ls_residual
 
-      n = nx1*ny1*nz1*nelt
-      n2 = nx2*ny2*nz2*nelt
+        n = nx1*ny1*nz1*nelt
+        n2 = nx2*ny2*nz2*nelt
 
-! Initialize arrays.
-      converged = .false.
-      tol = max(param(21),param(22))
+        uparam(01) = 3
+        ifpert = .true.
+        call bcast(ifpert, lsize)
 
-      call norm(rhs_x, rhs_y, rhs_z, rhs_p, rhs_t, rhs_norm)
+        ! Initialize arrays.
+        tol = max(param(21),param(22))
+        converged = .false.
 
-      do i = 1, k_dim+1
-         call oprzero(qx(:, i), qy(:, i), qz(:, i))
-         call rzero(qp(:, i), n2)
-         call rzero(qt(:, i), n)
-      enddo
+        qx = 0.0D+00 ; qy = 0.0D+00 ; qz = 0.0D+00 ; qp = 0.0D+00 ; qt = 0.0D+00
+        H = 0.0D+00 ; yvec = 0.0D+00 ; evec = 0.0D+00
 
-      uparam(01) = 3
-      ifpert = .true.
-      call bcast(ifpert, lsize)
+        call opcopy(qx(:, 1), qy(:, 1), qz(:, 1), rhs_x, rhs_y, rhs_z)
+        call copy(qp(:, 1), rhs_p, n2) ; call copy(qt(:, 1), rhs_t, n)
 
-      i = 0
-      do while ((.not. converged) .and. (i < maxiter))
+        call normalize(qx(:, 1), qy(:, 1), qz(:, 1), qp(:, 1), qt(:, 1), beta)
 
-! --> Initialize arrays.
-         H = 0.0D+00
-         yvec = 0.0D+00
-         evec = 0.0D+00
+        i = 0
+        do while ((.not. converged) .and. (i < maxiter))
 
+          ! -->
+          H = 0.0D+00 ; yvec = 0.0D+00
+          evec = 0.0D+00 ; evec(1) = beta
 
-!         --> Generate seed for the Krylov subspace.
-         call opcopy(qx(:, 1), qy(:, 1), qz(:, 1), sol_x, sol_y, sol_z)
-         call copy(qp(:, 1), sol_p, n2)
-         call copy(qt(:, 1), sol_t, n)
-         call initialize_gmres_vector(beta, qx(:, 1), qy(:, 1), qz(:, 1), qp(:, 1), qt(:, 1), rhs_x, rhs_y, rhs_z, rhs_p, rhs_t)
+          arnoldi : do k = 1, k_dim
+            ! --> Arnoldi factorization.
+            call arnoldi_factorization(qx, qy, qz, qp, qt, H, k, k)
+            ! --> Solve the least-squares problem.
+            call lstsq(H(1:k+1, 1:k), evec(1:k+1), yvec(1:k), k+1, k)
 
-         evec(1) = beta
+            ! --> Compute residual.
+            beta = norm2(evec(1:k+1) - matmul(H(1:k+1, 1:k), yvec(1:k)))
+            if (nid.eq.0) write(*, *) "Inner iteration residual : ", beta**2
 
-         if (nid .eq. 0) then
-            write(6, *) "GMRES ITERATION :", i
-            write(6, *) "GMRES RESIDUAL : ", beta**2 , (beta / rhs_norm)**2
-         endif
+            if (beta**2 .lt. tol) then
+              converged = .true.
+              exit arnoldi
+            endif
 
-! --> Check convergence.
-         if (((beta/rhs_norm)**2 .lt. tol) .or. (beta**2 .lt. tol)) then
-            converged = .true.
-         else
-!           --> Fill-in the Krylov subspace.
-            call arnoldi_factorization(qx, qy, qz, qp, qt, H, 1, k_dim)
+          enddo arnoldi
 
-!        --> Solve the least-squares problem.
-            call lstsq(H, evec, yvec, k_dim+1, k_dim)
+          ! --> Update solution.
+          sol_x = sol_x + matmul(qx(:, 1:k_dim), yvec)
+          sol_y = sol_y + matmul(qy(:, 1:k_dim), yvec)
+          sol_z = sol_z + matmul(qz(:, 1:k_dim), yvec)
+          sol_p = sol_p + matmul(qp(:, 1:k_dim), yvec)
+          sol_t = sol_t + matmul(qt(:, 1:k_dim), yvec)
 
-! --> Update the solution vector.
-            sol_x = sol_x + matmul(qx(:, 1:k_dim), yvec)
-            sol_y = sol_y + matmul(qy(:, 1:k_dim), yvec)
-            sol_z = sol_z + matmul(qz(:, 1:k_dim), yvec)
-            sol_p = sol_p + matmul(qp(:, 1:k_dim), yvec)
-            sol_t = sol_t + matmul(qt(:, 1:k_dim), yvec)
-         endif
+          ! --> Recompute residual.
+          call opcopy(qx(:, 1), qy(:, 1), qz(:, 1), sol_x, sol_y, sol_z)
+          call copy(qp(:, 1), sol_p, n2) ; call copy(qt(:, 1), sol_t, n)
+          call initialize_gmres_vector(beta, qx(:, 1), qy(:, 1), qz(:, 1), qp(:, 1), qt(:, 1), rhs_x, rhs_y, rhs_z, rhs_p, rhs_t)
 
-! --> Update counter of GMRES restarts.
-         i = i + 1
+          if (nid.EQ.0) write(*, *) "GMRES --- Iteration : ", i, " residual : ", beta**2
+          if (beta**2 .lt. tol) converged = .true.
+          i = i+1
 
-      enddo
+        enddo
 
-      uparam(01) = 1
-      ifpert = .false.
+        uparam(01) = 1
+        ifpert = .false.
 
-      return
+        return
       end subroutine time_stepper_gmres
 
 
@@ -259,7 +255,7 @@
 
 
 
-      subroutine initialize_gmres_vector(alpha, qx, qy, qz, qp, qt, rhs_x, rhs_y, rhs_z, rhs_p, rhs_t)
+      subroutine initialize_gmres_vector(beta, qx, qy, qz, qp, qt, rhs_x, rhs_y, rhs_z, rhs_p, rhs_t)
 
       implicit none
       include 'SIZE'
@@ -273,9 +269,9 @@
       real, dimension(lt2) :: rhs_p
 
 ! ----- Seed for the GMRES Krylov subspace (output) -----
-      real, dimension(lt) :: qx, qy, qz, qt, fx, fy, fz, ft
-      real, dimension(lt2) :: qp, fp
-      real :: alpha
+      real, dimension(lt) :: qx, qy, qz, qt, f_x, f_y, f_z, f_t
+      real, dimension(lt2) :: qp, f_p
+      real :: beta
       integer :: n, n2
 
 ! Initialize stuff.
@@ -283,24 +279,14 @@
       n2 = nx2*ny2*nz2*nelt
 
 ! --> Initial Krylov vector.
-      call matrix_vector_product(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
-
-      call opsub2(fx, fy, fz, rhs_x, rhs_y, rhs_z)
-      call sub2(fp, rhs_p, n2)
-      call sub2(ft, rhs_t, n)
-
-! --> Change the sign to r = b - Ax.
-      call chsign(fx, n)
-      call chsign(fy, n)
-      call chsign(fz, n)
-      call chsign(ft, n)
-      call chsign(fp, n2)
+      call matrix_vector_product(f_x, f_y, f_z, f_p, f_t, qx, qy, qz, qp, qt)
+      f_x = rhs_x - f_x ; f_y = rhs_y - f_y ; f_z = rhs_z - f_z
+      f_p = rhs_p - f_p ; f_t = rhs_t - f_t
 
 ! --> Normalize the starting vector.
-      call normalize(fx, fy, fz, fp, ft, alpha)
-      call opcopy(qx, qy, qz, fx, fy, fz)
-      call copy(qp, fp, n2)
-      call copy(qt, ft, n)
+      call normalize(f_x, f_y, f_z, f_p, f_t, beta)
+      call opcopy(qx, qy, qz, f_x, f_y, f_z)
+      call copy(qp, f_p, n2) ; call copy(qt, f_t, n)
 
       return
       end subroutine initialize_gmres_vector
@@ -314,7 +300,7 @@
 
 
 
-      subroutine forward_map(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
+      subroutine forward_map(f_x, f_y, f_z, f_p, f_t, qx, qy, qz, qp, qt)
 
       implicit none
       include 'SIZE'
@@ -328,8 +314,8 @@
       real, dimension(lt2) :: qp
 
 ! ----- Right-hand side of the Newton.
-      real, dimension(lt) :: fx, fy, fz, ft
-      real, dimension(lt2) :: fp
+      real, dimension(lt) :: f_x, f_y, f_z, f_t
+      real, dimension(lt2) :: f_p
 
       integer :: n, n2
 
@@ -353,19 +339,19 @@
       enddo
 
 ! --> Compute the right hand side of the time-stepper Newton.
-      call opcopy(fx, fy, fz, vx, vy, vz)
-      call copy(fp, pr, n2)
-      call copy(ft, t, n)
+      call opcopy(f_x, f_y, f_z, vx, vy, vz)
+      call copy(f_p, pr, n2)
+      call copy(f_t, t, n)
 
-      call opsub2(fx, fy, fz, qx, qy, qz)
-      call sub2(fp, qp, n2)
-      call sub2(ft, qt, n)
+      call opsub2(f_x, f_y, f_z, qx, qy, qz)
+      call sub2(f_p, qp, n2)
+      call sub2(f_t, qt, n)
 
-      call chsign(fx, n)
-      call chsign(fy, n)
-      call chsign(fz, n)
-      call chsign(fp, n2)
-      call chsign(ft, n)
+      call chsign(f_x, n)
+      call chsign(f_y, n)
+      call chsign(f_z, n)
+      call chsign(f_p, n2)
+      call chsign(f_t, n)
 
       return
       end subroutine forward_map
