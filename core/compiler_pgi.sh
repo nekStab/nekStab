f398aeca3410108154617f98dae89ad14c341cf4
ricardofrantz <rasfrantz@gmail.com>
2021-03-26 17:59:19 +0100

    removing double entry

--- core/compiler_pgi.sh
+++ core/compiler_pgi.sh
@@ -6,7 +6,6 @@ export MANPATH=$MANPATH:/opt/pgi/linux86-64/19.10/man;
 export LM_LICENSE_FILE=$LM_LICENSE_FILE:/opt/pgi/license.dat; 
 
 ## PGI COMPILER
-source ${NEKSTAB_SOURCE_ROOT}/core/nekStab.sh
 export FC="pgfortran"
 export CC="pgcc"
 export FFLAGS="-mcmodel=medium -g -traceback"
