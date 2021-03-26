8b30163380de5dd6faad55f3ecddf7d6be3a28e2
Jean-Christophe <loiseau.jc@gmail.com>
2021-03-26 11:15:33 +0100

    Update newton_krylov.f
    Cleaned-up the code.

--- core/newton_krylov.f
+++ core/newton_krylov.f
@@ -6,10 +6,19 @@
 
       subroutine newton_krylov()
 
-!     Implementation of a simple Newton-Krylov solver for fixed point
-!     computation using a time-stepper formulation. The resolution of
-!     the linear system for each Newton iteration is sovled by means
-!     of GMRES.
+        !     Implementation of a simple Newton-Krylov solver for fixed point
+        !     computation using a time-stepper formulation. The resolution of
+        !     the linear system for each Newton iteration is sovled by means
+        !     of GMRES.
+        !
+        !     INPUTS
+        !     ------
+        !
+        !     OUTPUTS
+        !     -------
+        !
+        !
+        !     Last edited : March 26th by JC Loiseau.
 
       implicit none
       include 'SIZE'
@@ -22,10 +31,11 @@
       real, dimension(lt) :: f_x, f_y, f_z, f_t
       real, dimension(lt2) :: f_p
 
-! -----
+! ----- Current estimate of the solution.
       real, dimension(lt) :: qx, qy, qz, qt
       real, dimension(lt2) :: qp
 
+      ! ----- Newton correction obtained from GMRES.
       real, dimension(lt) :: dqx, dqy, dqz, dqt
       real, dimension(lt2) :: dqp
 
@@ -33,77 +43,47 @@
       real :: tol, residual
       integer :: maxiter_newton, maxiter_gmres
       integer :: i, j, k, n, n2
-      logical :: converged
 
-      n = nx1*ny1*nz1*nelt
-      n2 = nx2*ny2*nz2*nelt
-
-      maxiter_newton = 10
-      maxiter_gmres = 10
-      converged = .false.
-      tol = 1e-8
+      n = nx1*ny1*nz1*nelt ; n2 = nx2*ny2*nz2*nelt
+      maxiter_newton = 10 ; maxiter_gmres = 10
+      tol = max(param(21), param(22))
 
 ! --> Initialize arrays.
       call oprzero(f_x, f_y, f_z)
-      call rzero(f_p, n2)
-      call rzero(f_t, n)
+      call rzero(f_p, n2) ; call rzero(f_t, n)
 
 ! --> Copy initial guess for the Newton solver.
       call opcopy(qx, qy, qz, vx, vy, vz)
-      call copy(qt, t, n)
-      call copy(qp, pr, n2)
+      call copy(qt, t, n) ; call copy(qp, pr, n2)
 
 ! --> Newton iteration.
-      i = 0
-      do while ((.not. converged) .and. (i < maxiter_newton))
+      newton : do i = 1, maxiter_newton
 
-         time = i
-         call outpost(qx, qy, qz, qp, qt, "nwt")
+        ! --> Outpost current estimate of the solution
+         time = i ; call outpost(qx, qy, qz, qp, qt, "nwt")
 
-! --> Compute rhs of Newton iteration f(q).
+         ! --> Compute rhs of Newton iteration f(q).
          call forward_map(f_x, f_y, f_z, f_p, f_t, qx, qy, qz, qp, qt)
 
-! --> Check residual || f(q) ||
-         call norm(f_x, f_y, f_z, f_p, f_t, residual)
-         residual = residual ** 2
+         ! --> Check residual || f(q) ||
+         call norm(f_x, f_y, f_z, f_p, f_t, residual) ; residual = residual ** 2
 
-         if (nid .eq. 0) then
-            write(6, *) "NEWTON --- Iteration :", i, " residual :", residual
-         endif
+         if (nid .eq. 0) write(6, *) "NEWTON --- Iteration :", i, " residual :", residual
+         if (residual .lt. tol) exit newton
 
-         if (residual .lt. max(param(21),param(22))) then
-            converged = .true.
-         else
-            write(*, *) "LINEAR SOLVER"
-! --> Copy the current guess into the base flow.
-            call opcopy(ubase, vbase, wbase, qx, qy, qz)
-            call copy(tbase, t, n)
+         if (nid.eq.0) write(*, *) "LINEAR SOLVER"
+         ! --> Copy the current guess into the base flow.
+         call opcopy(ubase, vbase, wbase, qx, qy, qz) ; call copy(tbase, t, n)
 
-! --> Zero-initial guess for the solution of the linear system.
-            call oprzero(dqx, dqy, dqz)
-            call rzero(dqp, n2)
-            call rzero(dqt, n)
+         ! --> Solve the linear system.
+         call time_stepper_gmres(f_x, f_y, f_z, f_p, f_t, dqx, dqy, dqz, dqp, dqt, maxiter_gmres)
 
-! --> Solve the linear system.
-            param(12) = -abs(param(12)) !freeze dt
-            param(31) = 1 ; npert = param(31)
-            call bcast(param,200*wdsize) !broadcast all parameters to processors
+         ! --> Update Newton solution.
+         call opsub2(qx, qy, qz, dqx, dqy, dqz) ; call sub2(qp, dqp, n2) ; call sub2(qt, dqt, n)
 
-            call time_stepper_gmres(f_x, f_y, f_z, f_p, f_t, dqx, dqy, dqz, dqp, dqt, maxiter_gmres)
-            ifpert = .false.
-            call bcast(ifpert, lsize)
+       enddo newton
 
-! --> Update Newton solution.
-            call opsub2(qx, qy, qz, dqx, dqy, dqz)
-            call sub2(qp, dqp, n2)
-            call sub2(qt, dqt, n)
-
-         end if
-
-! -->
-         i = i + 1
-      enddo
-
+       ! --> Outpost solution.
       call outpost(qx, qy, qz, qp, qt, "NBF")
 
       return
@@ -147,7 +127,7 @@
         !     NOTE : This is a plain implementation of GMRES following the algorithm given in
         !     Y. Saad. Iterative methods for sparse linear systems. Section 6.5 GMRES, alg. 6.9
         !
-        !     Last Edit : March 22nd 2021 by JC Loiseau.
+        !     Last Edit : March 26th 2021 by JC Loiseau.
         !
 
         implicit none
@@ -176,72 +156,63 @@
 
         !     ----- Miscellaneous.
         integer :: i, j, k, maxiter, n, n2
-        logical :: converged
-        real :: beta, residual, tol, rhs_norm, ls_residual
+        real :: beta, tol
 
-        n = nx1*ny1*nz1*nelt
-        n2 = nx2*ny2*nz2*nelt
-
-        uparam(01) = 3
-        ifpert = .true.
-        call bcast(ifpert, lsize)
+        n = nx1*ny1*nz1*nelt ; n2 = nx2*ny2*nz2*nelt
+        uparam(01) = 3 ; ifpert = .true. ; call bcast(ifpert, lsize)
 
         ! Initialize arrays.
-        tol = max(param(21),param(22))
-        converged = .false.
+        tol = max(param(21), param(22))
 
         qx = 0.0D+00 ; qy = 0.0D+00 ; qz = 0.0D+00 ; qp = 0.0D+00 ; qt = 0.0D+00
+        sol_x = 0.0D+00 ; sol_y = 0.0D+00 ; sol_z = 0.0D+00 ; sol_p = 0.0D+00 ; sol_t = 0.0D+00
         H = 0.0D+00 ; yvec = 0.0D+00 ; evec = 0.0D+00
 
         call opcopy(qx(:, 1), qy(:, 1), qz(:, 1), rhs_x, rhs_y, rhs_z)
         call copy(qp(:, 1), rhs_p, n2) ; call copy(qt(:, 1), rhs_t, n)
-
         call normalize(qx(:, 1), qy(:, 1), qz(:, 1), qp(:, 1), qt(:, 1), beta)
 
-        i = 0
-        do while ((.not. converged) .and. (i < maxiter))
+        gmres : do i = 1, maxiter
 
-          ! -->
+          ! --> Zero-out stuff.
           H = 0.0D+00 ; yvec = 0.0D+00
           evec = 0.0D+00 ; evec(1) = beta
+
+          qx(:, 2:k_dim+1) = 0.0D+00 ; qy(:, 2:k_dim+1) = 0.0D+00 ; qz(:, 2:k_dim+1) = 0.0D+00
+          qp(:, 2:k_dim+1) = 0.0D+00 ; qt(:, 2:k_dim+1) = 0.0D+00
 
           arnoldi : do k = 1, k_dim
             ! --> Arnoldi factorization.
             call arnoldi_factorization(qx, qy, qz, qp, qt, H, k, k)
-            ! --> Solve the least-squares problem.
+
+            ! --> Least-squares problem.
             call lstsq(H(1:k+1, 1:k), evec(1:k+1), yvec(1:k), k+1, k)
 
             ! --> Compute residual.
             beta = norm2(evec(1:k+1) - matmul(H(1:k+1, 1:k), yvec(1:k)))
             if (nid.eq.0) write(*, *) "Inner iteration residual : ", beta**2
-
-            if (beta**2 .lt. tol) then
-              converged = .true.
-              exit arnoldi
-            endif
+            if (beta**2 .lt. tol) exit arnoldi
 
           enddo arnoldi
 
           ! --> Update solution.
-          sol_x = sol_x + matmul(qx(:, 1:k_dim), yvec)
-          sol_y = sol_y + matmul(qy(:, 1:k_dim), yvec)
-          sol_z = sol_z + matmul(qz(:, 1:k_dim), yvec)
-          sol_p = sol_p + matmul(qp(:, 1:k_dim), yvec)
-          sol_t = sol_t + matmul(qt(:, 1:k_dim), yvec)
+          sol_x = sol_x + matmul(qx(:, 1:k), yvec(1:k))
+          sol_y = sol_y + matmul(qy(:, 1:k), yvec(1:k))
+          sol_z = sol_z + matmul(qz(:, 1:k), yvec(1:k))
+          sol_p = sol_p + matmul(qp(:, 1:k), yvec(1:k))
+          sol_t = sol_t + matmul(qt(:, 1:k), yvec(1:k))
 
-          ! --> Recompute residual.
+          ! --> Recompute residual for sanity check and initialize new Krylov seed if needed.
           call opcopy(qx(:, 1), qy(:, 1), qz(:, 1), sol_x, sol_y, sol_z)
           call copy(qp(:, 1), sol_p, n2) ; call copy(qt(:, 1), sol_t, n)
           call initialize_gmres_vector(beta, qx(:, 1), qy(:, 1), qz(:, 1), qp(:, 1), qt(:, 1), rhs_x, rhs_y, rhs_z, rhs_p, rhs_t)
 
           if (nid.EQ.0) write(*, *) "GMRES --- Iteration : ", i, " residual : ", beta**2
-          if (beta**2 .lt. tol) converged = .true.
-          i = i+1
+          if (beta**2 .lt. tol) exit gmres
 
-        enddo
+        enddo gmres
 
-        uparam(01) = 1
-        ifpert = .false.
+        uparam(01) = 1 ; ifpert = .false. ; call bcast(ifpert, lsize)
 
         return
       end subroutine time_stepper_gmres
@@ -274,9 +245,7 @@
       real :: beta
       integer :: n, n2
 
-! Initialize stuff.
-      n = nx1*ny1*nz1*nelt
-      n2 = nx2*ny2*nz2*nelt
+      n = nx1*ny1*nz1*nelt ; n2 = nx2*ny2*nz2*nelt
 
 ! --> Initial Krylov vector.
       call matrix_vector_product(f_x, f_y, f_z, f_p, f_t, qx, qy, qz, qp, qt)
@@ -319,18 +288,14 @@
 
       integer :: n, n2
 
-
-      n = nx1*ny1*nz1*nelt
-      n2 = nx2*ny2*nz2*nelt
+      n = nx1*ny1*nz1*nelt ; n2 = nx2*ny2*nz2*nelt
 
 ! --> Copy the initial condition to Nek.
       call opcopy(vx, vy, vz, qx, qy, qz)
-      call copy(t, qt, n)
-      call copy(pr, qp, n2)
+      call copy(t, qt, n) ; call copy(pr, qp, n2)
 
 ! --> Turn-off the linearized solver.
-      ifpert = .false.
-      call bcast(ifpert, lsize)
+      ifpert = .false. ; call bcast(ifpert, lsize)
 
 ! --> Run the simulation forward.
       do istep = 1, nsteps
@@ -340,18 +305,13 @@
 
 ! --> Compute the right hand side of the time-stepper Newton.
       call opcopy(f_x, f_y, f_z, vx, vy, vz)
-      call copy(f_p, pr, n2)
-      call copy(f_t, t, n)
+      call copy(f_p, pr, n2) ; call copy(f_t, t, n)
 
       call opsub2(f_x, f_y, f_z, qx, qy, qz)
-      call sub2(f_p, qp, n2)
-      call sub2(f_t, qt, n)
+      call sub2(f_p, qp, n2) ;  call sub2(f_t, qt, n)
 
-      call chsign(f_x, n)
-      call chsign(f_y, n)
-      call chsign(f_z, n)
-      call chsign(f_p, n2)
-      call chsign(f_t, n)
+      call chsign(f_x, n) ; call chsign(f_y, n) ; call chsign(f_z, n)
+      call chsign(f_p, n2) ; call chsign(f_t, n)
 
       return
       end subroutine forward_map
