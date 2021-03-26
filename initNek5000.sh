635603784cf049418fcf7662a40906e14d63cd3b
ricardofrantz <rasfrantz@gmail.com>
2021-03-26 12:05:40 +0100

    Revert "adding fast initilization script"
    This reverts commit e4bdba3ae1332f51e457d765eaf3d3cbfc3a7ed3.

--- initNek5000.sh
+++
@@ -1,8 +0,0 @@
-
-sudo apt-get -y install libmpich-dev mpich libopenblas-dev build-essential cmake m4
-
-git submodule update --init --recursive
-
-cd ~/nekStab/Nek5000/tools
-
-./maketools genmap genbox
