5cf01de29fd6299b74d788e096250e2240129dfa
Jean-Christophe <loiseau.jc@gmail.com>
2021-03-31 06:49:04 +0200

    Update matvec.f

--- core/matvec.f
+++ core/matvec.f
@@ -3,24 +3,24 @@
       include 'SIZE'
       include 'TOTAL'
 
-! --> Force only single perturbation mode.
+!     --> Force only single perturbation mode.
       if (param(31) .gt. 1) then
          write(6, *) 'nekStab not ready for npert > 1 -- jp loops not yet implemented. Stoppiing.'
          call nek_end
       endif
       param(31) = 1 ; npert = param(31)
 
-! --> Force deactivate OIFS.
+!     --> Force deactivate OIFS.
       if (ifchar) write(6, *) 'OIFS not working with linearized solver. Turning it off.'
       ifchar = .false. ; call bcast(ifchar, lsize)
 
-! --> Encore CFL target for EXTk
+!     --> Encore CFL target for EXTk
       if (param(26) .gt. 0.5) then
          if (nid .eq. 0) write(6, *) "Force the maximum CFL to 0.5 for practical reasons."
          param(26) = 0.5D+00 ; ctarg = param(26)
       endif
 
-! --> Set nsteps/endTime accordingly.
+!     --> Set nsteps/endTime accordingly.
       if (param(10) .gt. 0) then
          call compute_cfl(dt, vx, vy, vz, 1.0)
          dt = ctarg / dt
@@ -33,14 +33,20 @@
          param(12) = dt
       endif
 
-! --> Force constant time step.
+!     --> Force constant time step.
       param(12) = -abs(param(12))
 
-! --> Broadcast parameters.
+!     --> Broadcast parameters.
       call bcast(param, 200*wdsize)
 
       return
       end subroutine prepare_linearized_solver
+
+
+
+
+
+!-----------------------------------------------------------------------
 
 
 
@@ -61,39 +67,49 @@
       real, dimension(lt) :: fx, fy, fz, ft
       real, dimension(lt2) :: fp
 
-      real :: umax
       integer :: n
 
-! --> Pass the baseflow to vx, vy, vz
+      n = nx1*ny1*nz1*nelt
+
+!     --> Pass the baseflow to vx, vy, vz
       call opcopy(vx, vy, vz, ubase, vbase, wbase)
       if (ifheat) call copy(t, tbase, n)
 
-! --> Standard setup for the linearized solver.
+!     --> Standard setup for the linearized solver.
       call prepare_linearized_solver()
 
-! --> Direct solver only.
+!     --> Direct solver only.
       if (uparam(01) .eq. 3) then
-          evop = 'd'
-          call forward_linearized_map(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
+         evop = 'd'
+         call forward_linearized_map(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
       endif
 
-! --> Adjoint solver only.
+!     --> Adjoint solver only.
       if (uparam(01) .eq. 3.2) then
-          evop = 'a'
-          call adjoint_linearized_map(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
+         evop = 'a'
+         call adjoint_linearized_map(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
       endif
 
-! --> Direct-Adjoint for optimal transient growth.
+!     --> Direct-Adjoint for optimal transient growth.
 
-! --> Linearized forward map for the Newton-Krylov solver.
+!     --> Linearized forward map for the Newton-Krylov solver.
       if (floor(uparam(01)) .eq. 1) then
-          call newton_linearized_map(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
+         call newton_linearized_map(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
       endif
 
-! --> Adjoint solver for the steady force sensitivity analysis.
+!     --> Adjoint solver for the steady force sensitivity analysis.
+      if (floor(uparam(01)) .eq. 4) then
+         call ts_force_sensitivity_map(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
+      end if
 
       return
       end subroutine matvec
+
+
+
+
+
+!-----------------------------------------------------------------------
 
 
 
@@ -114,24 +130,19 @@
       real, dimension(lt) :: fx, fy, fz, ft
       real, dimension(lt2) :: fp
 
-      real :: umax
-      integer :: n
-
-      n = nx1*ny1*nz1*nelt
-
-! --> Setup the parameters for the linearized solver.
+!     --> Setup the parameters for the linearized solver.
       ifpert = .true. ; ifadj = .false.
       call bcast(ifpert, lsize) ; call bcast(ifadj, lsize)
 
-! --> Turning-off the base flow side-by-side computation. Need to change for Floquet.
+!     --> Turning-off the base flow side-by-side computation. Need to change for Floquet.
       ifbase = .false.
 
-! --> Pass the initial condition for the perturbation.
+!     --> Pass the initial condition for the perturbation.
       call nopcopy(vxp(:, 1), vyp(:, 1), vzp(:, 1), prp(:, 1), tp(:, 1, 1), qx, qy, qz, qp, qt)
 
       time = 0.0D+00
       do istep = 1, nsteps
-! --> Output current info to logfile.
+!     --> Output current info to logfile.
          if(nid .eq. 0) write(6,"(' DIRECT:',I6,'/',I6,' from',I6,'/',I6,' (',I3,')')") istep, nsteps, mstep, k_dim, schur_cnt
 
          ! --> Integrate forward in time.
@@ -139,11 +150,17 @@
          call nek_advance()
       end do
 
-! --> Copy the solution.
+!     --> Copy the solution.
       call nopcopy(fx, fy, fz, fp, ft, vxp(:, 1), vyp(:, 1), vzp(:, 1), prp(:, 1), tp(:, 1, 1))
 
       return
       end subroutine forward_linearized_map
+
+
+
+
+
+!-----------------------------------------------------------------------
 
 
 
@@ -164,24 +181,19 @@
       real, dimension(lt) :: fx, fy, fz, ft
       real, dimension(lt2) :: fp
 
-      real :: umax
-      integer :: n
-
-      n = nx1*ny1*nz1*nelt
-
-! --> Setup the parameters for the linearized solver.
+!     --> Setup the parameters for the linearized solver.
       ifpert = .true. ; ifadj = .true.
       call bcast(ifpert, lsize) ; call bcast(ifadj, lsize)
 
-! --> Turning-off base flow computation.
+!     --> Turning-off base flow computation.
       ifbase = .false.
 
-! --> Pass the initial condition for the perturbation.
+!     --> Pass the initial condition for the perturbation.
       call nopcopy(vxp(:, 1), vyp(:, 1), vzp(:, 1), prp(:, 1), tp(:, 1, 1), qx, qy, qz, qp, qt)
 
       time = 0.0D+00
       do istep = 1, nsteps
-! --> Output current info to logfile.
+!     --> Output current info to logfile.
          if(nid .eq. 0) write(6,"(' ADJOINT:',I6,'/',I6,' from',I6,'/',I6,' (',I3,')')") istep, nsteps, mstep, k_dim, schur_cnt
 
          ! --> Integrate forward in time.
@@ -189,11 +201,17 @@
          call nek_advance()
       end do
 
-! --> Copy the solution.
+!     --> Copy the solution.
       call nopcopy(fx, fy, fz, fp, ft, vxp(:, 1), vyp(:, 1), vzp(:, 1), prp(:, 1), tp(:, 1, 1))
 
       return
       end subroutine adjoint_linearized_map
+
+
+
+
+
+!-----------------------------------------------------------------------
 
 
 
@@ -213,15 +231,47 @@
       real, dimension(lt) :: fx, fy, fz, ft
       real, dimension(lt2) :: fp
 
-      real :: umax
-      integer :: n
-
-! --> Evaluate exp(t*L) * q0.
+!     --> Evaluate exp(t*L) * q0.
       call forward_linearized_map(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
 
-! --> Evaluate (I - exp(t*L)) * q0.
+!     --> Evaluate (I - exp(t*L)) * q0.
       call nopsub2(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
       call nopchsign(fx, fy, fz, fp, ft)
 
       return
       end subroutine newton_linearized_map
+
+
+
+
+
+!-----------------------------------------------------------------------
+
+
+
+
+
+      subroutine ts_force_sensitivity_map(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
+      implicit none
+      include 'SIZE'
+      include 'TOTAL'
+      include 'ADJOINT'
+
+      integer, parameter :: lt = lx1*ly1*lz1*lelt
+      integer, parameter :: lt2 = lx2*ly2*lz2*lelt
+
+      real, dimension(lt) :: qx, qy, qz, qt
+      real, dimension(lt2) :: qp
+
+      real, dimension(lt) :: fx, fy, fz, ft
+      real, dimension(lt2) :: fp
+
+!     --> Evaluate exp(t*L) * q0.
+      call adjoint_linearized_map(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
+
+!     --> Evaluate (I - exp(t*L)) * q0.
+      call nopsub2(fx, fy, fz, fp, ft, qx, qy, qz, qp, qt)
+      call nopchsign(fx, fy, fz, fp, ft)
+
+      return
+      end subroutine ts_force_sensitivity_map
