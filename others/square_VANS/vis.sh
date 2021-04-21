a50c80064cd8d937259717e8e3586ebb1506b514
ricardofrantz <rasfrantz@gmail.com>
2021-04-21 12:29:21 +0200

    VANS working!

--- others/square_VANS/vis.sh
+++ others/square_VANS/vis.sh
@@ -1,7 +1,8 @@
-visnek d0ssquare_p
-visnek d1ssquare_p
-visnek d2ssquare_p
+visnek
+visnek epssquare_p
 visnek cofsquare_p
 visnek Darsquare_p
 visnek Fo_square_p
 visnek VSfsquare_p
+visnek d1ssquare_p
+visnek d2ssquare_p
