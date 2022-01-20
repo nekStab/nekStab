0c17e8392c7cc3d3ba536c8c957fccd089efeff7
Ricardo A. S. Frantz <rasfrantz@gmail.com>
2022-01-20 21:27:31 +0100

    updating compiler options

--- core/compiler_gcc_tb.sh
+++ core/compiler_gcc_tb.sh
@@ -8,8 +8,7 @@ set -e
 export FC="mpif77"
 export CC="mpicc"
 export FFLAGS="-mcmodel=large -march=native -ffixed-line-length-none -w -Wall -g -fbacktrace"
-#export FFLAGS="-mcmodel=large -march=native -ffixed-line-length-none -g -Wall -Wextra -Warray-temporaries -Wconversion -fbacktrace -ffree-line-length-0 -fcheck=all -ffpe-trap=invalid,zero,overflow,underflow -finit-real=nan"
-
+#export FFLAGS="-Og -ffixed-line-length-none -fbacktrace -Wall -Wextra -Warray-bounds -Warray-temporaries -Wconversion -Wno-argument-mismatch -fcheck=all -ffpe-trap=invalid,zero,overflow,underflow -finit-real=nan -ggdb -fsanitize=leak -fsanitize=address"
 export USR_LFLAGS+="-L/usr/lib -lopenblas"
 
 source ${NEKSTAB_SOURCE_ROOT}/core/nekStab.sh
--- core/compiler_oneap_tb.sh
+++ core/compiler_oneap_tb.sh
@@ -3,7 +3,8 @@
 ## INTEL COMPILER
 export FC="mpiifort"
 export CC="mpiicc"
-export FFLAGS="-mcmodel=medium -shared-intel -extend-source -mkl -g -traceback -fpe0 -debug extended"
+#export FFLAGS="-mcmodel=medium -shared-intel -extend-source -qmkl -g -traceback -fpe0 -debug extended"
+export FFLAGS="-mcmodel=medium -shared-intel -extend-source -qmkl -g -traceback -debug extended"
 
 # OPTIONAL FLAG
 export USR_LFLAGS+="-I${MKLROOT}/include -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_sequential.a ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -lpthread -lm -ldl" #optional link for old versions
