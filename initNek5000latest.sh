8a57bcb9ee34e90b51c4a97e41df747940472f84
ricardofrantz <rasfrantz@gmail.com>
2022-01-21 14:23:04 +0100

    removing duplicated script

--- initNek5000latest.sh
+++
@@ -1,9 +0,0 @@
-#sudo apt-get -y install libmpich-dev mpich libopenblas-dev build-essential cmake m4
-
-git submodule update --init --recursive
-
-cd Nek5000
-git checkout master
-
-cd tools
-./maketools genmap genbox gmsh2nek 
