191b0e9181d649ca8796137a99a11625c4e11544
Ricardo A. S. Frantz <rasfrantz@gmail.com>
2022-01-21 02:52:18 +0100

    automatic compiler parameter selection
    simplifying flags selection given OS and arch,

--- core/compiler_intel.sh
+++ core/compiler_intel.sh
@@ -3,9 +3,10 @@
 ## INTEL COMPILER
 export FC="mpiifort"
 export CC="mpiicc"
-export FFLAGS="-mcmodel=medium -shared-intel -xHost -extend-source -mkl"
+export FFLAGS="-mcmodel=medium -shared-intel -extend-source -mkl"
+#export FFLAGS+="-g -traceback" # tracer 
+#export FFLAGS+="-fpe0 -debug extended" # debugger
 
-# OPTIONAL FLAG
 export USR_LFLAGS+="-I${MKLROOT}/include -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_sequential.a ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -lpthread -lm -ldl" #optional link for old versions
 
 source ${NEKSTAB_SOURCE_ROOT}/core/nekStab.sh
