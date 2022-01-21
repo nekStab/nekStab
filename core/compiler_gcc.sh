a3aa5fc921b8b5217dfc2b1038a32e54881cffa1
Ricardo A. S. Frantz <rasfrantz@gmail.com>
2022-01-21 02:52:18 +0100

    automatic compiler parameter selection
    simplifying flags selection given OS and arch,

--- core/compiler_gcc.sh
+++ core/compiler_gcc.sh
@@ -6,7 +6,45 @@
 
 export FC="mpif77"
 export CC="mpicc"
-export FFLAGS="-mcmodel=medium -march=native -ffixed-line-length-none -w -Wall"
-export USR_LFLAGS+="-L/usr/lib -lopenblas"
+export FFLAGS="-march=native -ffixed-line-length-none -w -Wall"
+
+os=""
+case $(uname -s) in
+    Darwin) os="Darwin" ;;
+    Linux)  os="Linux" ;;
+esac
+
+ar=""
+case $(uname -p) in
+    x86_64) ar="x86_64" ;;
+    arm)  ar="arm" ;;
+esac
+
+
+if [[ $os == "Linux"* ]]; then
+    echo "Compiling in Linux"
+    export FFLAGS+=" -mcmodel=large"
+    export USR_LFLAGS+="-L/usr/lib" # -lopenblas"
+
+elif [[ "$os" == "Darwin"* ]]; then
+
+    if [[ $ar == "x86_64"* ]]; then
+    echo "Compiling for Mac OS-x86-64 architecture"
+
+    elif [[ "$ar" == "arm"* ]]; then
+    echo "Compiling for Mac OS-ARM architecture"
+    export FFLAGS+=" -mcmodel=small"
+
+fi
+fi
+
+if [[ $(gfortran -dumpversion | cut -f1 -d.) -gt 10 ]]; then
+    export FFLAGS+=" -fallow-argument-mismatch"
+fi
+
+
+#export FFLAGS+="-g -fbacktrace" # tracer
+#export FFLAGS="-Og -Wextra -Warray-bounds -Warray-temporaries -Wconversion -Wno-argument-mismatch -fcheck=all -ffpe-trap=invalid,zero,overflow,underflow -finit-real=nan -ggdb -fsanitize=leak -fsanitize=address"
+
 
 source ${NEKSTAB_SOURCE_ROOT}/core/nekStab.sh
