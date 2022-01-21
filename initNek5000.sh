db535468f3cc54c1b76c10859c73c62b4bf8d5e8
Ricardo A. S. Frantz <rasfrantz@gmail.com>
2022-01-21 03:03:44 +0100

    update init script

--- initNek5000.sh
+++ initNek5000.sh
@@ -4,6 +4,7 @@ git submodule update --init --recursive
 
 cd Nek5000
 #git checkout v19
+git checkout master
 
 cd tools
-./maketools genmap genbox n2to3 gmsh2nek nekamg_setup
+./maketools genmap genbox # n2to3 gmsh2nek nekamg_setup
