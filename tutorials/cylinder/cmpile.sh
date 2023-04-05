75a18a83526ed4cdbe43660c6d2f1a43da148cae
ricardofrantz <rasfrantz@gmail.com>
2023-04-06 00:47:28 +0200

    new cat script for exports

--- tutorials/cylinder/cmpile.sh
+++ tutorials/cylinder/cmpile.sh
@@ -2,6 +2,7 @@
 
 export CASE="1cyl" #--> case name goes here
 
+#source path.sh
 #add to .bashrc
 export NEKSTAB_SOURCE_ROOT="../.." #--> path to nekStab
 export NEK_SOURCE_ROOT="$NEKSTAB_SOURCE_ROOT/Nek5000"
@@ -9,8 +10,8 @@ export PATH=$NEK_SOURCE_ROOT/bin:$PATH
 export PATH=$NEKSTAB_SOURCE_ROOT/bin:$PATH
 
 # For macOS error - not enough slots available
-export OMPI_MCA_btl_vader_backing_directory=/tmp
-export TMPDIR=/tmp
+#export OMPI_MCA_btl_vader_backing_directory=/tmp
+#export TMPDIR=/tmp
 
 #uncomment the deisred compiler
 source ${NEKSTAB_SOURCE_ROOT}/core/compiler.sh
