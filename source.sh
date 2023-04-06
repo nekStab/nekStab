fa556241363992fccc99ea4bf837576b58f5d773
ricardofrantz <rasfrantz@gmail.com>
2023-04-06 16:03:02 +0200

    removing duplicated file

--- source.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export NEKSTAB_SOURCE_ROOT=$(pwd) #--> path to nekStab
-export NEK_SOURCE_ROOT="$NEKSTAB_SOURCE_ROOT/Nek5000"
-export PATH=$NEK_SOURCE_ROOT/bin:$PATH
-export PATH=$NEKSTAB_SOURCE_ROOT/bin:$PATH
