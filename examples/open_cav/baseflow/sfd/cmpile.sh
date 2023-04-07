c48fcd20a36b6f7db5d0756780aedd725de411f3
Jean-Christophe <loiseau.jc@gmail.com>
2023-04-07 07:19:30 +0200

    Clean-up the examples

---
+++ examples/open_cav/baseflow/sfd/cmpile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export CASE="ocf" #--> case name goes here
+
+#add to .bashrc
+export NEKSTAB_SOURCE_ROOT="../../../../" #--> path to nekStab
+export NEK_SOURCE_ROOT="$NEKSTAB_SOURCE_ROOT/Nek5000"
+export PATH=$NEK_SOURCE_ROOT/bin:$PATH
+
+source ${NEKSTAB_SOURCE_ROOT}/core/compiler.sh
+
+args=("$@")
+argsnr=$#
+
+if [ $[argsnr] -ne 1 ]; then
+    error_quit 'Wrong arguments number!'
+fi
+
+for il in "$@"
+do
+case $il in
+        clean)
+                ${SOURCE_ROOT}/bin/makenek clean
+                shift
+                ;;
+        all)
+                ${SOURCE_ROOT}/bin/makenek ${CASE}
+                shift
+                ;;
+esac
+done
